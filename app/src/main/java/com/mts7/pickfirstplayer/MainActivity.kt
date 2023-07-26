3fc0fbb42d237a1fc108224539e84d1e666a3fd3
mts7 <github@mts7.com>
2023-07-25 20:38:33 -0400

    add UI tests for compose methods PFP-13

--- app/src/main/java/com/mts7/pickfirstplayer/MainActivity.kt
+++ app/src/main/java/com/mts7/pickfirstplayer/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -15,13 +14,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -57,6 +53,35 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+fun generateRandomNumber(maxValue: Int): Int {
+    return (1..maxValue).random()
+}
+
+fun getRelationalValues(maxCount: Int, player: Int): Pair<String, Int> {
+    if (player == 1) {
+        return Pair("self", 0)
+    }
+
+    val half = kotlin.math.ceil(maxCount.toDouble() / 2)
+
+    if (player > half) {
+        return Pair("right", maxCount + 1 - player)
+    }
+    return Pair("left", player - 1)
+}
+
+fun getRelationalWording(direction: String, places: Int): String {
+    if (places == 0) {
+        return "You go first."
+    }
+
+    if (places == 1) {
+        return "The player on your $direction goes first."
+    }
+
+    return "The player $places to your $direction goes first."
+}
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(onExit: () -> Unit) {
@@ -78,7 +103,7 @@ fun MainScreen(onExit: () -> Unit) {
                 numberOfPlayers.value,
                 generateRandomNumber(numberOfPlayers.value)
             )
-            DisplayRandomPlayer(
+            ResultScreen(
                 maxValue = numberOfPlayers.value,
                 direction = direction,
                 places = places
@@ -87,6 +112,12 @@ fun MainScreen(onExit: () -> Unit) {
             MainLayout(onNumberClick = { numberOfPlayers.value = it })
         }
     }
+}
+
+@Preview
+@Composable
+fun PreviewMainScreen() {
+    MainScreen(onExit = {})
 }
 
 @Composable
@@ -128,18 +159,14 @@ fun BottomBar(onExit: () -> Unit, displayReset: Boolean, onResetClick: () -> Uni
         ) {
             if (displayReset) {
                 ElevatedButton(onClick = onResetClick) {
-                    Icon(
-                        imageVector = Icons.Default.ArrowBack,
-                        contentDescription = "Reset",
-                        modifier = Modifier.padding(end = 4.dp)
-                    )
                     Text(
                         text = "Reset",
-                        fontSize = 24.sp
+                        fontSize = 24.sp,
+                        modifier = Modifier.padding(horizontal = 24.dp)
                     )
                 }
                 Spacer(
-                    modifier = Modifier.width(64.dp)
+                    modifier = Modifier.width(16.dp)
                 )
             }
             ElevatedButton(
@@ -149,6 +176,7 @@ fun BottomBar(onExit: () -> Unit, displayReset: Boolean, onResetClick: () -> Uni
                 Text(
                     text = "Exit",
                     fontSize = 24.sp,
+                    modifier = Modifier.padding(horizontal = 24.dp)
                 )
             }
             Image(
@@ -179,12 +207,12 @@ fun MainLayout(onNumberClick: (Int) -> Unit) {
                 text = "Tap the number of players.",
                 color = MaterialTheme.colorScheme.secondary,
                 fontSize = 32.sp,
-                lineHeight = 50.sp,
+                lineHeight = 32.sp,
                 modifier = Modifier.padding(horizontal = 8.dp),
                 textAlign = TextAlign.Center,
             )
             Spacer(modifier = Modifier.height(24.dp))
-            ButtonGrid(numbers = listOf(2, 3, 4, 5, 6, 7), onNumberClick)
+            ButtonGrid(onNumberClick)
         }
     }
 }
@@ -196,55 +224,53 @@ fun PreviewMainLayout() {
 }
 
 @Composable
-fun ButtonGrid(numbers: List<Int>, onNumberClick: (Int) -> Unit) {
+fun ButtonRow(numbers: List<Int>, onNumberClick: (Int) -> Unit) {
+    Row {
+        NumberButton(value = numbers[0], onNumberClick)
+        Spacer(
+            modifier = Modifier.padding(
+                horizontal = 16.dp,
+            )
+        )
+        NumberButton(value = numbers[1], onNumberClick)
+        Spacer(modifier = Modifier.padding(horizontal = 8.dp))
+    }
+}
+
+@Preview
+@Composable
+fun PreviewButtonRow() {
+    ButtonRow(numbers = listOf(3, 4), onNumberClick = {})
+}
+
+@Composable
+fun ButtonGrid(onNumberClick: (Int) -> Unit) {
+    val numbers = listOf(2, 3, 4, 5, 6, 7)
+
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier.fillMaxWidth()
     ) {
-        Row {
-            NumberButton(value = numbers[0], onNumberClick)
-            Spacer(
-                modifier = Modifier.padding(
-                    horizontal = 24.dp,
-                )
-            )
-            NumberButton(value = numbers[1], onNumberClick)
-        }
+        ButtonRow(numbers = listOf(numbers[0], numbers[1]), onNumberClick)
         Spacer(
             modifier = Modifier.padding(
                 vertical = 8.dp,
             )
         )
-        Row {
-            NumberButton(value = numbers[2], onNumberClick)
-            Spacer(
-                modifier = Modifier.padding(
-                    horizontal = 24.dp,
-                )
-            )
-            NumberButton(value = numbers[3], onNumberClick)
-        }
+        ButtonRow(numbers = listOf(numbers[2], numbers[3]), onNumberClick)
         Spacer(
             modifier = Modifier.padding(
                 vertical = 8.dp,
             )
         )
-        Row {
-            NumberButton(value = numbers[4], onNumberClick)
-            Spacer(
-                modifier = Modifier.padding(
-                    horizontal = 24.dp,
-                )
-            )
-            NumberButton(value = numbers[5], onNumberClick)
-        }
+        ButtonRow(numbers = listOf(numbers[4], numbers[5]), onNumberClick)
     }
 }
 
 @Preview
 @Composable
 fun PreviewButtonGrid() {
-    ButtonGrid(listOf(2, 3, 4, 5, 6, 7)) {}
+    ButtonGrid(onNumberClick = {})
 }
 
 @Composable
@@ -260,10 +286,10 @@ fun NumberButton(value: Int, onNumberClick: (Int) -> Unit) {
             color = MaterialTheme.colorScheme.surface,
             modifier = Modifier
                 .padding(
-                    horizontal = 24.dp,
-                    vertical = 4.dp,
+                    horizontal = 16.dp,
+                    vertical = 0.dp,
                 ),
-            fontSize = 96.sp,
+            fontSize = 72.sp,
             textAlign = TextAlign.Center,
         )
     }
@@ -275,106 +301,116 @@ fun PreviewNumberButton() {
     NumberButton(value = 7, onNumberClick = {})
 }
 
-fun generateRandomNumber(maxValue: Int): Int {
-    return (1..maxValue).random()
-}
-
-fun getRelationalValues(maxCount: Int, player: Int): Pair<String, Int> {
-    if (player == 1) {
-        return Pair("self", 0)
-    }
-
-    val half = kotlin.math.ceil(maxCount.toDouble() / 2)
-
-    if (player > half) {
-        return Pair("right", maxCount + 1 - player)
-    }
-    return Pair("left", player - 1)
-}
-
-fun getRelationalWording(direction: String, places: Int): String {
-    if (places == 0) {
-        return "You go first."
-    }
-
-    if (places == 1) {
-        return "The player on your $direction goes first."
-    }
-
-    return "The player $places to your $direction goes first."
-}
-
 @Composable
-fun DisplayRandomPlayer(maxValue: Int, direction: String, places: Int) {
+fun ChosenValue(maxValue: Int) {
+    Surface {
+        Text(
+            text = "Number of players: $maxValue",
+            fontSize = 24.sp,
+            modifier = Modifier.padding(horizontal = 48.dp),
+        )
+    }
+    Spacer(modifier = Modifier.height(48.dp))
+}
+
+@Preview
+@Composable
+fun PreviewChosenValue() {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier.fillMaxWidth(),
     ) {
-        Spacer(modifier = Modifier.height(128.dp))
-        Surface {
-            Text(
-                text = "Number of players",
-                fontSize = 32.sp,
-                modifier = Modifier.padding(horizontal = 48.dp),
-            )
-        }
-        Spacer(modifier = Modifier.height(48.dp))
-        Surface(
-            shape = RoundedCornerShape(15)
-        ) {
-            Text(
-                text = "$maxValue",
-                color = MaterialTheme.colorScheme.surface,
-                modifier = Modifier
-                    .background(color = MaterialTheme.colorScheme.secondary)
-                    .padding(
-                        horizontal = 24.dp,
-                        vertical = 4.dp,
-                    )
-                    .width(96.dp),
-                fontSize = 96.sp,
-                textAlign = TextAlign.Center,
-            )
-        }
-        Spacer(
-            modifier = Modifier.height(64.dp)
-        )
+        ChosenValue(maxValue = 5)
+    }
+}
+
+@Composable
+fun PlayerDirection(direction: String, places: Int) {
+    val rotation = when (direction) {
+        "left" -> 180.0F
+        "self" -> 90.0F
+        else -> 0.0F
+    }
+
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        shape = RoundedCornerShape(15)
+    ) {
         Row(
-            verticalAlignment = Alignment.CenterVertically
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(
+                horizontal = 24.dp,
+                vertical = if (places == 0) 24.dp else 0.dp
+            )
         ) {
             Image(
                 painter = painterResource(id = R.drawable.right_arrow_44621),
                 contentDescription = "<a href=\"https://www.freepik.com/icon/right-arrow_44621\">Icon by Freepik</a>",
                 modifier = Modifier
                     .size(64.dp)
-                    .rotate(
-                        when (direction) {
-                            "left" -> 180.0F
-                            "self" -> 90.0F
-                            else -> 0.0F
-                        }
-                    ),
+                    .rotate(rotation)
             )
-            Spacer(
-                modifier = Modifier.width(24.dp)
-            )
-            Text(
-                text = if (places > 0) "$places" else "",
-                fontSize = 96.sp,
-            )
+            if (places > 0) {
+                Spacer(
+                    modifier = Modifier.width(24.dp)
+                )
+                Text(
+                    text = "$places",
+                    fontSize = 96.sp,
+                )
+            }
         }
-        Text(
-            text = getRelationalWording(direction, places),
-            modifier = Modifier.padding(24.dp),
-            fontSize = 32.sp,
-            lineHeight = 32.sp,
-            textAlign = TextAlign.Center
-        )
+    }
+    Text(
+        text = getRelationalWording(direction, places),
+        modifier = Modifier
+            .padding(24.dp)
+            .width(144.dp),
+        fontSize = 24.sp,
+        lineHeight = 24.sp,
+        textAlign = TextAlign.Center
+    )
+}
+
+@Preview
+@Composable
+fun PreviewPlayerDirectionOther() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxWidth(),
+    ) {
+        PlayerDirection("left", 4)
     }
 }
 
 @Preview
 @Composable
-fun PreviewDisplayRandomPlayer() {
-    DisplayRandomPlayer(maxValue = 6, direction = "left", places = 3)
+fun PreviewPlayerDirectionSelf() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxWidth(),
+    ) {
+        PlayerDirection("self", 0)
+    }
+}
+
+@Composable
+fun ResultScreen(maxValue: Int, direction: String, places: Int) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxWidth(),
+    ) {
+        Spacer(modifier = Modifier.height(128.dp))
+        ChosenValue(maxValue)
+        Spacer(
+            modifier = Modifier.height(64.dp)
+        )
+        PlayerDirection(direction, places)
+    }
+}
+
+@Preview
+@Composable
+fun PreviewResultScreen() {
+    ResultScreen(maxValue = 6, direction = "left", places = 3)
 }
