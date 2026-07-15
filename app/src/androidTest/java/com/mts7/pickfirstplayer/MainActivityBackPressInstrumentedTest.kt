75ae2f32ffbe4e2af1cc745c07b9a11172c183dd
mts7 <github@mts7.com>
2026-07-15 14:45:02 -0400

    update images, change test, remove dead task

--- app/src/androidTest/java/com/mts7/pickfirstplayer/MainActivityBackPressInstrumentedTest.kt
+++ app/src/androidTest/java/com/mts7/pickfirstplayer/MainActivityBackPressInstrumentedTest.kt
@@ -1,12 +1,9 @@
 package com.mts7.pickfirstplayer
 
-import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.test.junit4.v2.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
-import androidx.compose.ui.test.onRoot
 import androidx.compose.ui.test.performClick
-import androidx.compose.ui.test.performKeyInput
-import androidx.compose.ui.test.pressKey
+import androidx.test.espresso.Espresso.pressBack
 import org.junit.Rule
 import org.junit.Test
 
@@ -19,9 +16,7 @@ class MainActivityBackPressInstrumentedTest {
         rule.onNodeWithText("5").performClick()
         rule.onNodeWithText("Number of players: 5").assertExists()
 
-        rule.onRoot().performKeyInput {
-            pressKey(Key.Back)
-        }
+        pressBack()
 
         rule.onNodeWithText("Tap the number of players.").assertExists()
     }
